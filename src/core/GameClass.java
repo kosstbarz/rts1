8f7ab190c10502e9ecd07a84cd6d863bf95848a9
Ta0o <dimasvasyatka@gmail.com>
2018-04-02 01:07:50 +0300

    45 line. i write command to change empty blank query but it doesn`t work.Also i add some commands but they work good.And i have a question about object destroying.If i understand well,i need to delete all links to this object but i don`t understand how to do it

---
+++ src/core/GameClass.java
@@ -0,0 +1,49 @@
+package core;
+import java.lang.String;
+import java.util.Scanner;
+
+
+
+public class GameClass {
+    static String command;
+    static public void main(String[]args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("hello!");
+        System.out.println("Welcome to my first game.");
+        System.out.println("This is my first experience in game developing.");
+        System.out.println("There are some commands, which help you to play.");
+        System.out.println("Write (/rules) to see all the commands.");
+
+        command = input.nextLine();
+        System.out.println(command);
+
+        if (command.equals("/rules")) {
+            System.out.println("Write (/create game) to create game field ");
+            System.out.println("If you want to end game write(/end game");
+            System.out.println("if you want to begin tutorial write (/start tutorial");
+            System.out.println("if you want to exit game press cross in the upper right conner or write(/exit game)");
+
+        }
+        if(command.equals("/create game")){
+            Object Game=new Object();
+            System.out.println("Game has been created.");
+            command = input.nextLine();
+            if (command.equals("/end game")){
+
+            }
+        }
+
+        if(command.equals("/start tutorial")){
+           Object GameTutorial=new Object();
+           System.out.println("Tutorial has been created.");
+           command = input.nextLine();
+        }
+        if(command.equals("/exit game")){
+            System.exit(0);
+
+        }
+        if(command == null){
+            System.out.println("Empty text.Try again.");
+        }
+    }
+}
