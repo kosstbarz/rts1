bbfb86bd202db8de88f205d04607e4c5ae31a8e3
Ta0o <dimasvasyatka@gmail.com>
2018-04-05 21:27:42 +0300

    Some changes.

--- src/core/GameClass.java
+++ src/core/GameClass.java
@@ -1,4 +1,6 @@
 package core;
+
+
 import java.lang.String;
 import java.util.Scanner;
 
@@ -16,34 +18,32 @@ public class GameClass {
 
         command = input.nextLine();
         System.out.println(command);
+        switch(command) {
+            case "":
+                System.out.println("Empty text.Try again.");
+            default:
+                if (command.equals("/rules")) {
+                    System.out.println("Write (/create game) to create game field ");
+                    System.out.println("If you want to end game write(/end game");
+                    System.out.println("if you want to begin tutorial write (/start tutorial");
+                    System.out.println("if you want to exit game press cross in the upper right conner or write(/exit game)");
 
-        if (command.equals("/rules")) {
-            System.out.println("Write (/create game) to create game field ");
-            System.out.println("If you want to end game write(/end game");
-            System.out.println("if you want to begin tutorial write (/start tutorial");
-            System.out.println("if you want to exit game press cross in the upper right conner or write(/exit game)");
+                }
+                if (command.equals("/create game")) {
+                    Object Game = new Object();
+                    System.out.println("Game has been created.");
+                    command = input.nextLine();
+                    }
+                }
 
-        }
-        if(command.equals("/create game")){
-            Object Game=new Object();
-            System.out.println("Game has been created.");
-            command = input.nextLine();
-            if (command.equals("/end game")){
+                if (command.equals("/start tutorial")) {
+                    Object GameTutorial = new Object();
+                    System.out.println("Tutorial has been created.");
+                    command = input.nextLine();
+                }
+                if (command.equals("/exit game")) {
+                    System.exit(0);
 
-            }
+                }
         }
-
-        if(command.equals("/start tutorial")){
-           Object GameTutorial=new Object();
-           System.out.println("Tutorial has been created.");
-           command = input.nextLine();
         }
-        if(command.equals("/exit game")){
-            System.exit(0);
-
-        }
-        if(command == null){
-            System.out.println("Empty text.Try again.");
-        }
-    }
-}
