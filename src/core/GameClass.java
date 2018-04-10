a1bc65b583839fb46b6bb7a5c657fc5a30f85c79
Ta0o <dimasvasyatka@gmail.com>
2018-04-10 17:03:49 +0300

    Change to switch construction+ try to change formalization.

--- src/core/GameClass.java
+++ src/core/GameClass.java
@@ -1,48 +1,67 @@
 package core;
 
-
 import java.lang.String;
 import java.util.Scanner;
 
-
-
 public class GameClass {
+
     static String command;
     private static int a=1;
+
     static public void main(String[]args) {
 
-        Scanner input = new Scanner(System.in);
-        System.out.println("hello!");
+        System.out.println("Hello!");
         System.out.println("Welcome to my first game.");
         System.out.println("This is my first experience in game developing.");
         System.out.println("There are some commands, which help you to play.");
         System.out.println("Write (/rules) to see all the commands.");
 
+        Scanner input = new Scanner(System.in);
+
         while (a == 1) {
+
         command = input.nextLine();
         System.out.println(command);
+
             switch(command) {
+
                 case "":
+
                     System.out.println("Empty text.Try again.");
-                default:
-                    if (command.equals("/rules")) {
+
+                case "/rules": {
+
                         System.out.println("Write (/create game) to create game field ");
-                        System.out.println("If you want to end game write(/end game");
-                        System.out.println("if you want to begin tutorial write (/start tutorial");
+                        System.out.println("If you want to end game write(/end game)");
+                        System.out.println("if you want to begin tutorial write (/start tutorial)");
                         System.out.println("if you want to exit game press cross in the upper right conner or write(/exit game)");
                     }
-                    if (command.equals("/create game")) {
+
+                case "/create game": {
+
                         Object Game = new Object();
                         System.out.println("Game has been created.");
                         command = input.nextLine();
                     }
-                    if (command.equals("/start tutorial")) {
+
+                case "/start tutorial": {
+
                         Object GameTutorial = new Object();
                         System.out.println("Tutorial has been created.");
                         command = input.nextLine();
                     }
-                    if (command.equals("/exit game")) {
-                        System.exit(0);
+
+                case "/exit game":    {
+
+                        System.out.println("Do really want to exit the game?(write (/yes)or(/no))");
+                        command = input.nextLine();
+                        switch (command){
+                            case "/yes":
+                                System.exit(0);
+                            case "/no":
+                                break;
+                        }
+
                     }
             }
         }
